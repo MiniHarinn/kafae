7bb7ed6fedaf7eb66b86fcf5af59c94bb2f0bfa7
Harinn <prinn.dev@pm.me>
2026-08-06 19:26:42 +0700

    feat(new): harinn template

---
+++ templates/harinn.cpp
@@ -0,0 +1,13 @@
+// {name}  {title}
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+using ll = long long;
+using ld = long double;
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+}
